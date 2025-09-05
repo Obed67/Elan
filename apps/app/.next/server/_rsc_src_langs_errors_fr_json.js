9d61e733366021f62650e43e4c0922a0aa0c8fd0
Sonagnon Obed AGBOHOUN <obedagbohouns@gmail.com>
2025-09-05 17:36:42 +0100

    init

---
+++ apps/app/.next/server/_rsc_src_langs_errors_fr_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_src_langs_errors_fr_json";
+exports.ids = ["_rsc_src_langs_errors_fr_json"];
+exports.modules = {
+
+/***/ "(rsc)/./src/langs/errors/fr.json":
+/*!**********************************!*\
+  !*** ./src/langs/errors/fr.json ***!
+  \**********************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"fileNotFound":"Fichier introuvable.","couldNotIdentifyIp":"Impossible d\'identifier votre adresse IP.","tooManyRequests":"Trop de requêtes. Veuillez réessayer dans quelques minutes.","tooManyAttempts":"Trop de tentatives. Veuillez réessayer dans quelques minutes.","otpInvalid":"Code OTP invalide.","otpSecretAlreadyExists":"Un secret OTP existe déjà.","otpSecretNotFound":"Secret OTP introuvable.","demoAdminCannotHaveOtpSecret":"L\'administrateur en mode démo ne peut pas avoir de secret OTP.","username":{"exist":"Ce nom d\'utilisateur existe déjà."},"email":{"exist":"Cet email existe déjà."},"unknownError":"Une erreur inconnue s\'est produite. Veuillez ressayer plus tard.","invalidCredentials":"Les informations d\'identification sont invalides.","unauthorized":"Vous avez été déconnecté. Veuillez vous reconnecter.","userNotFound":"Cet utilisateur n\'existe pas.","cannotDeleteAdmin":"Vous ne pouvez pas supprimer le compte administrateur.","accountAlreadyExists":"Un compte existe déjà avec cet email.","emailAlreadySentPleaseTryAgainInFewMinutes":"Un email a déjà été envoyé. Veuillez ressayer dans quelques minutes.","tokenNotFound":"Token invalide ou déjà utilisé.","tokenExpired":"Token a expiré.","emailNotVerified":"Veuillez vérifier votre email.","emailAlreadyVerified":"Email vérifié","emailServiceDisabled":"Le service d\'email est désactivé.","s3ServiceDisabled":"Le service S3 est désactivé.","registrationDisabled":"L\'inscription est désactivée.","cannotResetAdminPasswordInDemoMode":"Vous ne pouvez pas réinitialiser le mot de passe de l\'administrateur en mode démo.","userDoesNotHaveAPassword":"L\'utilisateur n\'a pas de mot de passe.","forbidden":"Accès interdit."}');
+
+/***/ })
+
+};
+;
